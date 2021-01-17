9f65fec6e0bbfba832e339065c1cd34112b5cc67
jonnebr <joaomgoncalvesbr@gmail.com>
2021-01-17 07:48:09 -0300

    social media icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { GiRazorBlade } from 'react-icons/gi';
 import { IconContext } from 'react-icons/lib';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { TiTimesOutline } from 'react-icons/ti';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { Button } from './Button';
 
 function Navbar() {
   const [click, setClick] = useState(true);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => {
     setClick(!click);
   };
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      console.log('pequeno');
+      setButton(false);
+    } else {
+      console.log('grande');
+
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
 
   return (
     <>
@@ -24,6 +44,47 @@ function Navbar() {
             <div onClick={handleClick} className="menu-icon">
               {click ? <GiHamburgerMenu /> : <TiTimesOutline />}
             </div>
+            <ul className="">
+              <li className="nav-item">
+                <Link to="/contact" className="nav-links">
+                  Contact
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/about" className="nav-links">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/services" className="nav-links">
+                  Services
+                </Link>
+              </li>
+
+              <Link to="/" className="nav-media">
+                <FaFacebook className="social-media-icons" />
+              </Link>
+              <Link to="/" className="nav-media">
+                <FaTwitter className="social-media-icons" />
+              </Link>
+              <Link to="/" className="nav-media">
+                <FaInstagram className="social-media-icons" />
+              </Link>
+
+              <li className="nav-btn">
+                {button ? (
+                  <Link to="/sign-up" className="btn-link">
+                    <Button buttonStyle="btn--outline">SIGN UP</Button>
+                  </Link>
+                ) : (
+                  <Link to="/sign-up" className="btn-link">
+                    <Button buttonStyle="btn--outline" buttonSize="btn--mobile">
+                      SIGN UP
+                    </Button>
+                  </Link>
+                )}
+              </li>
+            </ul>
           </div>
         </div>
       </IconContext.Provider>
