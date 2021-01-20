7e78671ed96778ac4e373a626b05e190499a9971
jonnebr <joaomgoncalvesbr@gmail.com>
2021-01-20 14:21:36 -0300

    homepage folder and herosection moved

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
+import Home from './components/pages/HomePage/Home';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route path="/" />
+        <Route path="/" exact component={Home} />
       </Switch>
-      <HeroSection />
+      {/* <HeroSection /> */}
     </Router>
   );
 }
