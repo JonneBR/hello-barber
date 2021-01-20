52584d36992380662e88e21dc162b964191485af
jonnebr <joaomgoncalvesbr@gmail.com>
2021-01-20 15:42:38 -0300

    title and description css style

---
+++ src/components/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <>
+      <div className="about-section">
+        <div className="container">
+          <h1 className="title heading">About</h1>
+          <div className="description-container">
+            <h1 className="description heading">
+              A Hello Barber vem ao mercado para inovar, realçar e oferecer os
+              melhores cortes para o público masculino. Nossa equipe de
+              profissionais/melhores barbeiros tem por objetivo alcançar o seu
+              melhor visual. Somos modernos no estilo e clássicos com a música.
+              Criamos um ambiente descontraido, que te dará conforto e boas
+              conversas enquanto você cuida melhor de si mesmo.
+            </h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default About;
--- src/components/pages/HomePage/Home.js
+++ src/components/pages/HomePage/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import About from '../../About';
 import HeroSection from '../../HeroSection';
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <About />
     </>
   );
 }
