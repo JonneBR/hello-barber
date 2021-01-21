d97cc55df0ad3371c3a3be8867cb43d7866f57ca
jonnebr <joaomgoncalvesbr@gmail.com>
2021-01-21 17:59:28 -0300

    horarios desktop css styled

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,6 +9,9 @@ function Contact() {
         <div className="items container">
           <div className="subject-container">
             <h1 className="subject heading">Horarios</h1>
+            <h1 className="description heading">Ter-Sex: 09:00 às 17:00</h1>
+            <h1 className="description heading">Sáb e Seg: Fechado</h1>
+            <h1 className="description heading">Dom: 09:00 às 12:00</h1>
           </div>
           <div className="subject-container">
             <h1 className="subject heading">Contato</h1>
