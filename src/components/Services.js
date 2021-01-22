7abb3366848d30e6b54ce3b60b47de0ce40c3a8b
jonnebr <joaomgoncalvesbr@gmail.com>
2021-01-22 12:56:59 -0300

    font css styles

--- src/components/Services.js
+++ src/components/Services.js
@@ -7,6 +7,25 @@ function Services() {
       <div className="services-section">
         <div className="container">
           <h1 className="title heading">Nossos Cortes</h1>
+          <div className="items container">
+            <div className="subject-container">
+              <h1 className="subject heading">Tradicional R$15,00</h1>
+              <h1 className="description heading">Ter-Sex: 09:00 às 17:00</h1>
+              <h1 className="description heading">Sáb e Seg: Fechado</h1>
+              <h1 className="description heading">Dom: 09:00 às 12:00</h1>
+            </div>
+            <div className="subject-container">
+              <h1 className="subject heading">Desenhado R$20,00</h1>
+              <h1 className="description heading">011981222544</h1>
+              <h1 className="description heading">hellobarber@gmail.com</h1>
+            </div>
+            <div className="subject-container">
+              <h1 className="subject heading">Barba R$15,00</h1>
+              <h1 className="description heading">Rua: Arlindo Flores</h1>
+              <h1 className="description heading">Jardim Vitória N: 136</h1>
+              <h1 className="description heading">Campinas-SP</h1>
+            </div>
+          </div>
         </div>
       </div>
     </>
