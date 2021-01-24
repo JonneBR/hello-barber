9d7078f4b38bccc3c9e41bf0ea0d3bfe88a4ce55
jonnebr <joaomgoncalvesbr@gmail.com>
2021-01-24 14:42:37 -0300

    footer items css style

--- src/components/pages/Footer/Footer.js
+++ src/components/pages/Footer/Footer.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import './Footer.css';
+import { IconContext } from 'react-icons/lib';
+import { IoLocationSharp } from 'react-icons/io5';
+import { AiOutlinePhone, AiOutlineMail } from 'react-icons/ai';
 
 function Footer() {
   return (
     <>
-      <div className="footer-container">
+      <IconContext.Provider value={{ color: '#fff' }}>
         <div className="footer-container">
-          <div className="footer-info-items">
-            <div className="item-address">
-              <p>
-                Rua: Arlindo Flores <br /> Jardim Vitória N: 136
-              </p>
+          <div className="footer-container-wrap">
+            <div className="footer-info-items">
+              <div className="item-address">
+                <IoLocationSharp className="location-icon" />
+                <p className="footer-item-description">
+                  Rua: Arlindo Flores <br /> Jardim Vitória N: 136
+                </p>
+              </div>
+              <div className="item-address">
+                <AiOutlinePhone className="location-icon" />
+                <p className="footer-item-description">011981222544</p>
+              </div>
+              <div className="item-address">
+                <AiOutlineMail className="location-icon" />
+                <p className="footer-item-description">hellobarber@gmail.com</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </IconContext.Provider>
     </>
   );
 }
